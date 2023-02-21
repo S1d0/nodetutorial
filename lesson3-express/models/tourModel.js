d4eef58e171b1d4ba5f0172b0eb57cc904931201
sebastian <idzkowski.s@gmail.com>
2023-02-21 12:43:41 +0100

    Embeded Users to Tour relation

--- lesson3-express/models/tourModel.js
+++ lesson3-express/models/tourModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./userModel")
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -59,6 +61,31 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -74,6 +101,13 @@ tourSchema.pre("save", function (next) {
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
+});
+
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises =  this.guides.map(async id => await User.findById(id))
+  this.guides = await Promise.all(guidesPromises)
+  next()
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
